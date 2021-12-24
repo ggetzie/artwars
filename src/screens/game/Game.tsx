f7a97bc4d6908dc971a08c72d2d270322b656cb5
gabe <ggetzie@gmail.com>
2021-12-24 11:47:57 +0800

    tab bar icons

--- src/screens/game/Game.tsx
+++ src/screens/game/Game.tsx
@@ -1,21 +1,50 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '..';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 import City from './City';
 import Portfolio from './Portfolio';
 import Collector from './Collector';
 import AuctionList from './AuctionList';
-const Tab = createBottomTabNavigator();
 
+const ACTIVE_COLOR = 'dodgerblue';
+const INACTIVE_COLOR = 'gray';
+
+const Tab = createBottomTabNavigator();
 type Props = NativeStackScreenProps<RootStackParamList, 'Game'>;
 
-const Game = (_: Props) => {
+const Game = ({route}: Props) => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          if (route.name === 'City') {
+            iconName = 'city';
+          } else if (route.name === 'Portfolio') {
+            iconName = 'briefcase';
+          } else if (route.name === 'Collector') {
+            iconName = 'user-tie';
+          } else if (route.name === 'Auctions') {
+            iconName = 'gavel';
+          } else {
+            iconName = 'question';
+          }
+
+          return (
+            <FontAwesome5
+              name={iconName}
+              color={focused ? ACTIVE_COLOR : INACTIVE_COLOR}
+              size={size}
+            />
+          );
+        },
+        tabBarActiveTintColor: ACTIVE_COLOR,
+        tabBarInactiveTintColor: INACTIVE_COLOR,
+        tabBarStyle: {paddingBottom: 3, paddingTop: 5},
+      })}>
       <Tab.Screen name="City" component={City} />
       <Tab.Screen name="Portfolio" component={Portfolio} />
       <Tab.Screen name="Collector" component={Collector} />
