2f66995a77cd844da378bfdeaa86d2c6e23bd948
gabe <ggetzie@gmail.com>
2022-02-23 11:21:43 +0800

    can't move under investigation

--- src/screens/game/portfolio/Detail.tsx
+++ src/screens/game/portfolio/Detail.tsx
@@ -1,16 +1,30 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, Image} from 'react-native';
 
 import {useAppSelector} from '../../../hooks';
-import {currentHot, getArtworkData} from '../../../reducers/game';
+import {
+  currentHot,
+  getArtworkData,
+  isUnderInvestigation,
+} from '../../../reducers/game';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {Picker} from '@react-native-picker/picker';
 import {PortfolioStackParamList} from '.';
 import BaseStyle from '../../../styles/base';
-import {ARTWORKS, Cities} from '../../../util';
+import {ARTWORKS, Cities, NPCImages} from '../../../util';
 import {CityName} from '../../../util/types';
 
 type Props = NativeStackScreenProps<PortfolioStackParamList, 'Detail'>;
+
+const Investigation = () => {
+  return (
+    <>
+      <Image style={{width: 75, height: 75}} source={NPCImages.irsAgent} />
+      <Text>The IRS is investigating your nefarious dealings!</Text>
+      <Text>You can't move art between cities until you're cleared.</Text>
+    </>
+  );
+};
 
 const Detail = ({navigation, route}: Props) => {
   const game = useAppSelector(state => state.game);
@@ -18,6 +32,7 @@ const Detail = ({navigation, route}: Props) => {
   const artwork = ARTWORKS[artworkId];
   const artworkData = getArtworkData(game, artworkId);
   const hot = currentHot(game);
+  const investigated = isUnderInvestigation(game);
   const value = Math.round(artworkData.currentValue).toLocaleString();
 
   useEffect(() => {
@@ -38,26 +53,32 @@ const Detail = ({navigation, route}: Props) => {
           {artwork.category}
         </Text>
       </Text>
-      <Text style={BaseStyle.pickerLabel}>Move to city</Text>
-      <Picker
-        accessibilityLabel="Move to city"
-        selectedValue={dest}
-        onValueChange={(itemValue: CityName, _) => {
-          setDest(itemValue);
-        }}>
-        {otherCities.map((c, i) => (
-          <Picker.Item key={i} label={c} value={c} />
-        ))}
-      </Picker>
-      <Button
-        title="Move"
-        onPress={() =>
-          navigation.navigate('Confirm', {
-            artworkId: artworkId,
-            destination: dest,
-          })
-        }
-      />
+      {investigated ? (
+        <Investigation />
+      ) : (
+        <>
+          <Text style={BaseStyle.pickerLabel}>Move to city</Text>
+          <Picker
+            accessibilityLabel="Move to city"
+            selectedValue={dest}
+            onValueChange={(itemValue: CityName, _) => {
+              setDest(itemValue);
+            }}>
+            {otherCities.map((c, i) => (
+              <Picker.Item key={i} label={c} value={c} />
+            ))}
+          </Picker>
+          <Button
+            title="Move"
+            onPress={() =>
+              navigation.navigate('Confirm', {
+                artworkId: artworkId,
+                destination: dest,
+              })
+            }
+          />
+        </>
+      )}
     </View>
   );
 };
